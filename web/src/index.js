8e0099dcf01d426d04d21c936bae8d718541534d
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-05-13 15:42:49 -0300

    web

---
+++ web/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/global.css';
+import Header from './pages/header';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
