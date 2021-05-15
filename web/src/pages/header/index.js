66e93f6ba2dfcd5dd9d5efad1457a7ba79f90f47
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-05-14 23:53:41 -0300

    registration page

--- web/src/pages/header/index.js
+++ web/src/pages/header/index.js
@@ -1,7 +1,35 @@
-const Header = () => {
-  return(
-   <h1>Header</h1>
-  );
-}
+import Job from "../../assets/job.svg";
+import "../header/styles.css";
+import "../../styles/global.css";
 
-export default Header
+const Header = () => {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="items">
+          <div className="itemsText">
+            <img src={Job} alt="" />
+            <h1>Search Jobs</h1>
+            <p>
+            O emprego que você procura está no SearchJobs!<br/>
+            Acredite, é verdade! Tornar uma vaga boa para os talentos,<br/> pode fazer com que eles se inscrevam mesmo sem<br/>estarempensando em mudar de emprego.
+            </p>
+          </div>
+          <div className="itemsForm">
+            <h1>Login</h1>
+            <p>Digite seus dados para entrar e acessar<br/> o conteúdo.</p>
+
+            <form action="">
+              <input type="email" placeholder="E-mail"/>
+              <input type="password" placeholder="Senha"/>
+              <button className="enter">Entrar</button>
+              <button className="cadastra">Ainda não tem conta? Cadastre-se.</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
